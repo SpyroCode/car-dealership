966b0bfacf13b4abc21dd3bd56a21f9590e0843f
Efren Almanza Lamas <efren.almanza@kingtide.com>
2023-05-30 19:12:35 -0600

    [ADD]ADD brands resorcees

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { Car } from './interfaces/car.interface';
 import { CreateCarDto } from './dto/create-car.dto';
